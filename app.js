5f9221c6964db6476ee2d18e59d3b55b59fd4af3
YANG LIU <>
2021-12-09 10:24:55 +0000

    finish

---
+++ app.js
@@ -0,0 +1,53 @@
+//Part One
+class Vehicle {
+    constructor(make,model,year){
+        this.make=make;
+        this.model=model;
+        this.year=year;
+    }
+    honk(){
+        return "Beep."
+    }
+    toString(){
+        return `The vehicle is a ${this.make} ${this.model} from ${this.year}`
+    }
+}
+
+//Part Two
+
+class Car extends Vehicle{
+    constructor(make,model,year,numWheels){
+        super(make,model,year);
+        this.numWheels=4;
+    }
+    
+}
+
+//Part Three
+class Motorcycle extends Vehicle{
+    constructor(make,model,year,numWheels){
+        super(make,model,year);
+        this.numWheels=2;
+}
+revEngine(){
+    return "VROOM!!!"
+}
+}
+
+//Part Four
+class Garage{
+    constructor(capacity){
+        this.vehicles=[];
+        this.capacity=capacity;
+    }
+    add(v) {
+        if (!(v instanceof Vehicle)) {
+          return "Only vehicles are allowed in here!";
+        }
+        if (this.vehicles.length >= this.capacity) {
+          return "Sorry, we're full.";
+        }garage.add(new Motorcycle("Honda", "Nighthawk", 2001))
+        this.vehicles.push(v);
+        return "Vehicle added!";
+      }
+}
